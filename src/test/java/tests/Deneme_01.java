a831e4ba214a71052f68d86c0e8bfa404e186f85
Elif <elfbyrk58@gmail.com>
2026-03-09 17:16:03 +0300

    Version1

--- src/test/java/tests/Deneme_01.java
+++ src/test/java/tests/Deneme_01.java
@@ -5,5 +5,6 @@ public class Deneme_01 {
     public static void main(String[] args) {
 
         System.out.println("Team-168 Otomasyon Projesi");
+        System.out.println("Elif Deneme");
     }
 }
