fbbf02eb41c62aee70975edac71fc4b717b4eb2a
Farkhod <farkhodjon11@gmail.com>
2026-03-10 23:57:49 +0200

    US16

--- src/test/java/tests/US16.java
+++ src/test/java/tests/US16.java
@@ -1,12 +1,15 @@
 package tests;
 
+import org.testng.annotations.Test;
+
 public class US16 {
     public static void main(String[] args) {
-        System.out.printf("Hadi BISMILLAH baslasinn");
+        System.out.printf("Hadi BISMILLAH baslasinn >>>>>>>>>");
 
-        System.out.printf("otomasyon");
+
 
 
     }
 
 }
+
