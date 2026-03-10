43d05d1a19643a0949edc39f1402644b8c8bca54
Fatih Boncuk <fboncukposta@gmail.com>
2026-03-10 16:06:26 +0100

    Pages classlari eklendi

---
+++ src/test/java/pages/LcfHomePage/SignButonsPage.java
@@ -0,0 +1,28 @@
+package pages.LcfHomePage;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class SignButonsPage {
+
+    public SignButonsPage(){
+
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    // Aşağıdaki locator ornektir
+    @FindBy(id = "global-search")
+    public WebElement aramaKutusu;
+
+
+    
+
+
+
+
+
+
+}
+
