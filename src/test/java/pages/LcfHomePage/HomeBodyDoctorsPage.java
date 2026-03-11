8738eaf79dd282859965468e972322b69e48390e
sinemenguc <snmngc@gmail.com>
2026-03-11 23:25:55 +0300

    HomeBodyDoctorsPage: US16 kapsamındaki doktorlar sayfası elementleri için locator tanımlamaları yapıldı.

--- src/test/java/pages/LcfHomePage/HomeBodyDoctorsPage.java
+++ src/test/java/pages/LcfHomePage/HomeBodyDoctorsPage.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
 
+import java.util.List;
+
 public class HomeBodyDoctorsPage {
 
     public HomeBodyDoctorsPage(){
@@ -16,13 +18,21 @@ public class HomeBodyDoctorsPage {
     @FindBy(id = "global-search")
     public WebElement aramaKutusu;
 
+    // Doktorlar sayfasındaki tüm doktorın kartları
+    @FindBy(xpath = "//div[@class='col-md-6']")
+    public List<WebElement> allDoctorCards;
 
-    
+    // Doktorlar sayfasındaki tüm doktorların isimleri
+    @FindBy(xpath = "//div[@class='wrapper']/h3/a")
+    public List<WebElement> allDoctorNames;
 
+    // Doktorlar sayfasındaki tüm doktorların bilgileri
+    @FindBy(xpath = "//div[@class='wrapper']/p")
+    public List<WebElement> allDoctorDetails;
 
-
-
-
+    // Doktorlar sayfasındaki tüm doktorların görselleri
+    @FindBy(xpath = "//div[@class='strip grid']//img")
+    public List<WebElement> allDoctorImages;
 
 }
 
