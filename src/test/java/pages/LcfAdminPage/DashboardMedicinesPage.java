1380747b2385017ebb3a5f75b4083878e9096a08
Beril Sude Çatak <beril0615@gmail.com>
2026-03-12 01:47:00 +0300

    US 1, US 23 ve US 34 locator kodları eklendi.

--- src/test/java/pages/LcfAdminPage/DashboardMedicinesPage.java
+++ src/test/java/pages/LcfAdminPage/DashboardMedicinesPage.java
@@ -12,9 +12,33 @@ public class DashboardMedicinesPage {
         PageFactory.initElements(Driver.getDriver(),this);
     }
 
-    // Aşağıdaki locator ornektir
-    @FindBy(id = "global-search")
-    public WebElement aramaKutusu;
+    // Sol menüde yer alan "Medicines" sayfasına yönlendirme linki
+    @FindBy(xpath = "//span[@class='title' and text()='Medicines']")
+    public WebElement sidebarMedicinesLink;
+
+    // İlaçlar sayfasında yeni ilaç eklemek için kullanılan "Create Medicines" butonu
+    @FindBy(xpath = "//a[text()='Create Medicines']")
+    public WebElement medicinesPageCreateButton;
+
+    // İlaç eklerken ilacın adının girildiği "Title" veri kutusu
+    @FindBy(xpath = "//input[@id='Title_en']")
+    public WebElement medicinesPageTitleInput;
+
+    // İlaç eklerken ilacın açıklamasının yazıldığı "Content" metin kutusu
+    @FindBy(xpath = "//input[@id='body_en']")
+    public WebElement medicinesPageContentTextMessage;
+
+    // İlacın dosyasının/fotoğrafının yüklendiği "File Drop Box" alanı
+    @FindBy(xpath = "//form[@id='dropzone']")
+    public WebElement medicinesPageFileDropBox;
+
+    // Girilen ilaç bilgilerini sisteme kaydetmek için kullanılan "Save Medicines" butonu
+    @FindBy(xpath = "//span[text()='Save Medicines']")
+    public WebElement medicinesPageSaveButton;
+
+
+
+
 
 
 
--- src/test/java/pages/LcfHomePage/HomeBodyPage.java
+++ src/test/java/pages/LcfHomePage/HomeBodyPage.java
@@ -12,10 +12,33 @@ public class HomeBodyPage {
         PageFactory.initElements(Driver.getDriver(),this);
     }
 
-    // Aşağıdaki locator ornektir
-    @FindBy(id = "global-search")
-    public WebElement aramaKutusu;
+    // Sitenin sol üst köşesinde yer alan, anasayfaya yönlendiren ana Logo
+    @FindBy(xpath = "//img[@alt='LoyalFriendCare']")
+    public WebElement headerLogo;
 
+    // Üst menüde yer alan "Home" linki
+    @FindBy(xpath = "//a[text()='Home']")
+    public WebElement navbarHomeLink;
+
+    // Üst menüde yer alan "About Us" linki
+    @FindBy(xpath = "//a[text()='About Us']")
+    public WebElement navbarAboutUsLink;
+
+    // Üst menüde yer alan "Departments" linki
+    @FindBy(xpath = "//a[text()='Departments']")
+    public WebElement navbarDepartmentsLink;
+
+    // Üst menüde yer alan "Doctors" linki
+    @FindBy(xpath = "//a[text()='Doctors']")
+    public WebElement navbarDoctorsLink;
+
+    // Üst menüde yer alan "Medicines" linki
+    @FindBy(xpath = "//a[text()='Medicines']")
+    public WebElement navbarMedicinesLink;
+
+    // Üst menüde yer alan "Vaccinations" linki
+    @FindBy(xpath = "//a[text()='Vaccinations']")
+    public WebElement navbarVaccinationsLink;
 
 
 
