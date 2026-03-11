03e7e0d89085eb4a015b83c9d8be6e7ab1239b13
Elif <elfbyrk58@gmail.com>
2026-03-11 17:46:03 +0300

    110326-LocatePages

--- src/test/java/pages/LcfHomePage/HomeBodySearchBoxPage.java
+++ src/test/java/pages/LcfHomePage/HomeBodySearchBoxPage.java
@@ -5,16 +5,32 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
 
+import java.util.List;
+
 public class HomeBodySearchBoxPage {
 
-    public HomeBodySearchBoxPage(){
+    public HomeBodySearchBoxPage() {
 
-        PageFactory.initElements(Driver.getDriver(),this);
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+         // Arama kutusu
+         @FindBy(xpath = "//*[@class='form-control']")
+         public WebElement searchBox;
+
+         // Arama (Büyüteç) butonu
+         @FindBy(xpath = "//*[@type='submit']")
+         public WebElement searchButton;
+
+         // Arama sonuç başlığı veya sonuç metni (Örn: "Search Results for...")
+         @FindBy(xpath = "//*[@class=\"img-fluid\"]")
+         public WebElement resultText;
+
+         // Hata mesajı (Özel karakter aramada çıkan uyarı)
+         @FindBy(xpath = "//*[contains(text(),'Please enter a search term')]")
+         public List<WebElement> errorMessageList;
+
     }
 
-    // Aşağıdaki locator ornektir
-    @FindBy(id = "global-search")
-    public WebElement aramaKutusu;
 
 
     
@@ -24,5 +40,5 @@ public class HomeBodySearchBoxPage {
 
 
 
-}
+
 
