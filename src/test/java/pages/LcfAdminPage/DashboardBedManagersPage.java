410e1601fecdf29c52f9a77f80f80e835edcf19d
Farkhod <farkhodjon11@gmail.com>
2026-03-12 23:56:55 +0200

    Farkhodjon

--- src/test/java/pages/LcfAdminPage/DashboardBedManagersPage.java
+++ src/test/java/pages/LcfAdminPage/DashboardBedManagersPage.java
@@ -16,8 +16,32 @@ public class DashboardBedManagersPage {
     @FindBy(id = "global-search")
     public WebElement aramaKutusu;
 
+    //BedManager sayfasi taitl text
+    @FindBy(tagName = "h3")
+    public WebElement bedMenegersPageTitleText;
+
+    //Bedmanagers Page Edit butonu
+    @FindBy(xpath = "(//a[@class='btn btn-complete btn-cons btn-animated from-left fa fa-edit'])[1]")
+    public WebElement bedManegerPageEditButon;
 
 
+
+    //BedManegers Page Title List
+    @FindBy(xpath = "//*[@id=\"tableWithSearch\"]/tbody/tr[*]/td[2]/p")
+    public WebElement bedMenegerPageListTitle;
+
+
+    //BedManeger Page Serach box
+    @FindBy(id = "search-table")
+    public WebElement getBedMenegersPageSerachBox;
+
+    //BedMeneger Page Delete Buton List
+    @FindBy(xpath = "//*[@id=\"tableWithSearch\"]/tbody/tr[*]/td[6]")
+    public WebElement getBedManegerPageDeleteButon;
+
+    //Bed Meneger Page  Image List
+    @FindBy(xpath = "    //*[@id=\"tableWithSearch\"]/tbody/tr[*]/td[1]/span\n")
+    public WebElement bedMenegerPageImageList;
 
 
 
