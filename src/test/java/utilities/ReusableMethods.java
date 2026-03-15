d6e1130b930e0a655c6d67d08411c0c1a208691d
Kerim <krmtrpn@gmail.com>
2026-03-16 02:12:46 +0300

    locates updates and completed US09_Tests

--- src/test/java/utilities/ReusableMethods.java
+++ src/test/java/utilities/ReusableMethods.java
@@ -9,6 +9,9 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.time.Duration;
@@ -242,5 +245,24 @@ public class ReusableMethods {
         WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(timeout));
         return wait.until(ExpectedConditions.elementToBeClickable(element));
     }
+
+
+    // Alert açıkken ekran görüntüsü alma
+    public static void alertVarkenScreenshot(String dosyaAdi) {
+        try {
+            LocalDateTime now = LocalDateTime.now();
+            String tarih = now.format(DateTimeFormatter.ofPattern("_yyMMdd_HHmmss"));
+            Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+            Rectangle rect = new Rectangle(screenSize);
+            BufferedImage image = new Robot().createScreenCapture(rect);
+            String dosyaYolu = "target/screenshots/" + dosyaAdi + tarih + ".png";
+            ImageIO.write(image, "png", new File(dosyaYolu));
+            System.out.println("Alert açıkken screenshot alindi: " + dosyaYolu);
+        } catch (Exception e) {
+            System.out.println("Alert anında screenshot alınamadı: " + e.getMessage());
+        }
+    }
+
+
 
 }
